b2fe093a601207fcbf0731156638bf70c801a5af
Burhan ARAS <burhan.aras@live.com>
2016-11-16 23:07:07 +0300

    Splash Screen implemented.

---
+++ app/src/main/java/com/burhan/udacity/popularmovies/ui/activity/SplashActivity.java
@@ -0,0 +1,75 @@
+/*
+ * Copyright 2016.  Emin Yahyayev
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.burhan.udacity.popularmovies.ui.activity;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+
+import com.burhan.udacity.popularmovies.R;
+
+import butterknife.ButterKnife;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        fullScreen();
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        ButterKnife.bind(this);
+    }
+
+    private void fullScreen() {
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+    }
+
+    private void startAnimation() {
+
+        Animation anim = AnimationUtils.loadAnimation(this, R.anim.translate);
+        anim.reset();
+        ImageView iv = (ImageView) findViewById(R.id.splash);
+        iv.clearAnimation();
+        iv.startAnimation(anim);
+
+        anim = AnimationUtils.loadAnimation(SplashActivity.this, R.anim.alpha);
+        anim.reset();
+        LinearLayout linearLayout = (LinearLayout) findViewById(R.id.rl_splash);
+        linearLayout.clearAnimation();
+        linearLayout.startAnimation(anim);
+
+
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(SplashActivity.this, BrowseMoviesActivity.class);
+                startActivity(intent);
+                SplashActivity.this.finish();
+            }
+        }, 5000);
+    }
+}
